4d7eb2c426bd3de3042d0c81406bf9fccadcf6b8
mauro <mauro_barone86@hotmail.com>
2021-04-30 22:56:26 -0300

    Arranco denuevo

---
+++ demo1/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Chofer
+from .models import Pasajero
+from .models import Tarjeta
+from .models import Insumo
+from .models import Lugar
+from .models import Combi
+from .models import Ruta
+from .models import Viaje
+from .models import Persona
+
+admin.site.register(Chofer)
+admin.site.register(Pasajero)
+admin.site.register(Tarjeta)
+admin.site.register(Insumo)
+admin.site.register(Lugar)
+admin.site.register(Combi)
+admin.site.register(Ruta)
+admin.site.register(Viaje)
+admin.site.register(Persona)
+
---
+++ demo1/models.py
@@ -0,0 +1,86 @@
+from django.db import models
+
+class Persona(models.Model):
+    usuario = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    dni = models.IntegerField(unique=True)
+    telefono = models.BigIntegerField()
+        
+    def __str__(self):
+        return '%s %s' % (self.usuario.first_name, self.usuario.last_name)
+
+class Chofer(Persona):
+   pass
+
+class Pasajero(Persona):
+    TIPOS_PLANES = (
+        ('B', 'Basic'),
+        ('G', 'Gold'),)
+    tipo = models.CharField(max_length=1, choices=TIPOS_PLANES)
+    fecha_de_Nacimiento = models.DateField()	    
+    
+class Tarjeta(models.Model):
+    pasajero = models.ForeignKey(Pasajero, on_delete=models.CASCADE)
+    numero = models.BigIntegerField(unique=True)
+    fecha_de_vencimiento = models.DateField()
+    codigo = models.IntegerField()
+    
+    def __str__(self):
+         return '%s %s' % (self.pasajero.usuario.first_name, str(self.numero))
+
+class Insumo(models.Model):
+    tarjeta = models.ForeignKey(Tarjeta, on_delete=models.CASCADE)
+    pasajero = models.ForeignKey(Pasajero, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=30)
+    tipo = models.CharField(max_length=30)
+    precio = models.FloatField()
+    
+    def __str__(self):
+        return '%s %s' % (self.nombre, self.pasajero.usuario.first_name)
+
+
+class Lugar(models.Model):
+    nombre_de_lugar = models.CharField(max_length=50)
+    provincia = models.CharField(max_length=40)
+    
+    def __str__(self):
+        return '%s %s' % (self.nombre_de_lugar, self.provincia)
+
+class Combi(models.Model):
+    TIPOS_COMBI = (
+        ('C', 'Cama'),
+        ('S', 'Semicama'),
+    )
+    chofer = models.ForeignKey(Chofer, on_delete=models.CASCADE)
+    patente = models.CharField(max_length=10)
+    tipo = models.CharField(max_length=1, choices=TIPOS_COMBI)
+    modelo = models.CharField(max_length=40)
+    asientos = models.IntegerField()
+        
+    def __str__(self):
+        return '%s %s' % (self.patente, self.chofer.usuario.first_name)
+
+class Ruta(models.Model):
+    combi = models.ForeignKey(Combi, on_delete=models.CASCADE)
+    origen = models.ForeignKey(Lugar, on_delete=models.CASCADE, related_name='origen')
+    destino = models.ForeignKey(Lugar, on_delete=models.CASCADE, related_name='destino')
+    hora = models.TimeField()
+    distancia = models.IntegerField()
+
+    def __str__(self):
+        return '%s %s %s' % (self.origen.nombre_de_lugar, self.destino.nombre_de_lugar, str(self.hora))
+
+class Viaje(models.Model):
+    ruta = models.ForeignKey(Ruta, on_delete=models.CASCADE)
+    insumos = models.ManyToManyField(Insumo)
+    fecha = models.DateField()
+    precio = models.FloatField()
+    asientos = models.IntegerField()
+    
+    def __str__(self):
+        return '%s %s' % (str(self.ruta), str(self.fecha))
+    
+    class Meta:
+        unique_together = ('ruta', 'fecha',)
+
+
+
