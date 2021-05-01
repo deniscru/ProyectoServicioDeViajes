bf19015a1f33f8db76e28c2d094e2fa9bf919d8d
mauro <mauro_barone86@hotmail.com>
2021-05-01 01:14:11 -0300

    pagina de inicio con datos

--- demo1/views.py
+++ demo1/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import Chofer
+from .models import Pasajero
+from .models import Tarjeta
+from .models import Insumo
+from .models import Lugar
+from .models import Combi
+from .models import Ruta
+from .models import Viaje
+from .models import Persona
 
 def principal(request):
-    return render(request, 'demo1/principal.html', {})
+    administrador = User.objects.filter(is_superuser=True)
+    choferes = Chofer.objects.all()
+    Personas = Persona.objects.all()
+    usuarios = User.objects.all()
+    pasajeros = Pasajero.objects.all()
+    tarjetas = Tarjeta.objects.all()
+    combis = Combi.objects.all()
+    Lugares = Chofer.objects.all()
+    insumos = Insumo.objects.all()
+    rutas = Ruta.objects.all()
+    viajes = Viaje.objects.all()
+
+    return render(request, 'demo1/principal.html', {'personas': Personas,'administrador': administrador, 'choferes': choferes, 'usuarios': usuarios, 'pasajeros': pasajeros, 'tarjetas': tarjetas, 'viajes': viajes, 'rutas': rutas, 'Lugares': Lugares, 'combis': combis, 'insumos': insumos})
