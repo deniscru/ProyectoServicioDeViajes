2a6ad096206226881e678353bc14924ef22a1c48
Denis Cruz sanchez <denis99lp@gmail.com>
2021-05-02 22:21:26 -0300

    agregar chofer

--- demo1/forms.py
+++ demo1/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 
-from .models import Lugar
-from .models import Pasajero
+from .models import Lugar, Pasajero
 from datetime import date
 
 class FormLugar(forms.Form):
@@ -16,13 +15,21 @@ class FormPasajero(forms.Form):
     tipos=[("GOLD","GOLD"),("BASICO","BÁSICO")]
     email=forms.EmailField(required=True)
     dni=forms.CharField(required=True,label="Dni",max_length=8,min_length=8)
-    telefono=forms.CharField(required=True,label="Teléfono",max_length=15)
     first_name=forms.CharField(required=True,max_length=30,label="Nombre")
     last_name=forms.CharField(required=True,max_length=30,label="Apellido")
-    fecha_de_nacimiento = forms.DateField(required=True,label='Fecha Nacimiento',widget=forms.SelectDateWidget(years=años))
     password = forms.CharField(required=True,label="Contraseña",min_length=6)
+    telefono=forms.CharField(required=True,label="Teléfono",max_length=15)
+    fecha_de_nacimiento = forms.DateField(required=True,label='Fecha Nacimiento',widget=forms.SelectDateWidget(years=años))
     tipo =forms.ChoiceField(widget=forms.RadioSelect, choices=tipos)
 
 class FormLogin(forms.Form):
     email=forms.EmailField(required=True,label="Email")
     password=forms.CharField(required=True,widget=forms.PasswordInput,label="Contraseña")
+
+class FormChofer(forms.Form):
+    email=forms.EmailField(required=True)
+    dni=forms.CharField(required=True,label="Dni", max_length=8,min_length=8)   
+    first_name=forms.CharField(required=True,max_length=30,label="Nombre")
+    last_name=forms.CharField(required=True,max_length=30,label="Apellido")
+    password = forms.CharField(required=True,label="Contraseña",min_length=6)
+    telefono=forms.CharField(required=True,label="Teléfono",max_length=15)
